ee81e4d954603cec1058a5e858ecb59c83d1b98c
Georgi Georgiev <vuryss@gmail.com>
2025-03-26 09:33:40 +0200

    feat: Adding support for associative array to OpenApi mapping

---
+++ tests/Datasets/Classes/InvalidVariant6.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dokky\Tests\Datasets\Classes;
+
+class InvalidVariant6
+{
+    /**
+     * @var array<int|string, Basic>
+     */
+    public array $multipleKeyTypes = [];
+}
